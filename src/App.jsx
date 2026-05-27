47809cec1c99801793fddc864fcfae5331f02ffa
millehlauridsen-ops <millehlauridsen@gmail.com>
2026-05-27 14:19:55 +0200

    Implement routing with React Router and create NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-/*import { useState } from "react";*/
 import "./css/App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 
-function App() {}
-
-export default App;
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>{" "}
+      <NavBar />
+    </>
+  );
+}
