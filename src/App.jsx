319a9d7bb2bc9f5583e2f5822527cb7a616249eb
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-05-28 09:36:19 +0200

    stier

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./css/App.css";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import ChatPage from "./pages/ChatPage";
+import ProfilePage from "./pages/ProfilePage";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>{" "}
       <NavBar />
     </>
