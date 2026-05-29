736d5653519742f1c04c831876a35728f2d2953f
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-05-29 10:54:12 +0200

    fetch af events til eventdisplay

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
+import "../css/Display.css";
 
-export default function EventCard({ bar }) {
+export default function EventCard({ event }) {
   return (
-    <Link to={`/${bar.event_name}`} className="event-card">
-      {bar.event_image ? (
-        <img src={bar.event_image} alt={bar.event_name} />
+    <Link to={`/${event.event_name}`} className="card">
+      {event.event_image ? (
+        <img src={event.event_image} alt={event.event_name} />
       ) : (
         <div className="image-placeholder">?</div>
       )}
-      <div className="eventcard-body">
-        <h2>{bar.event_name || "Intet event navn"}</h2>
-        <h2>{bar.event_time || "Ingen angiven tid"}</h2>
+      <div className="card-body">
+        <h2>{event.event_name || "Intet event navn"}</h2>
+        <h2>{event.event_time || "Ingen angiven tid"}</h2>
         <p>
-          {bar.name_bar || "Ingen angiven bar"} - {bar.type || ""}
+          {event.name_bar || "Ingen angiven bar"} - {event.type || ""}
         </p>
       </div>
     </Link>
