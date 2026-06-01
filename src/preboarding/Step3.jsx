ebfcd2d47fe39d7f7eeda20b553e9792f5893456
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-06-01 10:25:33 +0200

    step 3, lottie filer importeret

--- src/preboarding/Step3.jsx
+++ src/preboarding/Step3.jsx
@@ -1,8 +1,27 @@
+import "../preboarding/css/Preboarding.css";
+import Lottie from "lottie-react";
+
+import carousel1 from "../animations/carousel1.json";
+import carousel2 from "../animations/carousel2.json";
+import carousel3 from "../animations/carousel3.json";
+
 export default function Step3({ onNext }) {
   return (
     <div className="step">
-      <h2>Step 3</h2>
-      <button onClick={onNext}>Næste</button>
+      <div className="top">
+        <h1>Find events</h1>
+      </div>
+      <div className="middle">
+        <Lottie animationData={carousel1} loop={true} />
+        <Lottie animationData={carousel2} loop={true} />
+        <Lottie animationData={carousel3} loop={true} />
+        <h3>
+          Kig i vores event cards og find den helt rigtige aktivitet til dig
+        </h3>
+      </div>
+      <div className="bottom">
+        <button onClick={onNext}>Næste</button>
+      </div>
     </div>
   );
 }
