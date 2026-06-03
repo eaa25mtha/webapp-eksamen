809453eef437383018ec29bff9cde88f3e5fe65c
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-06-03 11:39:25 +0200

    toggle baren på søgesiden med css

--- src/components/SearchPageContent.jsx
+++ src/components/SearchPageContent.jsx
@@ -5,21 +5,42 @@ import SearchBar from "./SearchBar";
 import "../css/SearchBar.css";
 
 export default function SearchContent() {
-  const [display, setDisplay] = useState(true);
+  const [display, setDisplay] = useState("map");
   const [query, setQuery] = useState("");
 
-  function toggleDisplay() {
-    setDisplay(!display);
+  // skift til event
+  function toggleDisplayEvent() {
+    setDisplay("bar");
+  }
+
+  // skift til kort
+  function toggleDisplayMap() {
+    setDisplay("map");
   }
 
   return (
-    //event/kort toggle knappen
+    //event/kort toggle
     <div>
       <SearchBar query={query} setQuery={setQuery} />
-      <button onClick={toggleDisplay}>
-        <h2>Event</h2>
-      </button>
-      {display ? (
+
+      <div className="toggle-container">
+        {/* css class der kun vises når bar er aktiv */}
+        <button
+          className={`toggle-button ${display === "bar" ? "active" : ""}`}
+          onClick={toggleDisplayEvent}
+        >
+          <h3>Barer</h3>
+        </button>
+        <button
+          className={`toggle-button ${display === "map" ? "active" : ""}`}
+          onClick={toggleDisplayMap}
+        >
+          <h3>Kort</h3>
+        </button>
+      </div>
+
+      {/* renderingen af kortet */}
+      {display === "map" ? (
         <MapDisplay query={query} />
       ) : (
         <SearchBarDisplay query={query} />
