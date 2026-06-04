2438cf0f7939b363f851946a62248c07396faac2
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-06-04 10:42:17 +0200

    Update base path in Vite config for production deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import pkg from "./package.json";
 
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "serve" ? "/" : pkg.base,
+    base: command === "serve" ? "/" : "/webapp-eksamen/",
   };
 });
