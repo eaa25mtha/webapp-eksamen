bfcc92d647531cfe62644d8e65027ecdf6f4a34e
Mathilde Tilsted Hansen <eaa25mtha@students.eaaa.dk>
2026-06-04 10:47:34 +0200

    Refactor Vite config to simplify base path handling

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    base: command === "serve" ? "/" : "/webapp-eksamen/",
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/webapp-eksamen/",
 });
