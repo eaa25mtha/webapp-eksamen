bb07a3fd5e76a55a527bb2e3cf12316f27af6751
millehlauridsen-ops <millehlauridsen@gmail.com>
2026-06-04 11:33:33 +0200

    Fix template literals for icon paths in BarDetailPage

--- src/pages/BarDetailPage.jsx
+++ src/pages/BarDetailPage.jsx
@@ -36,55 +36,55 @@ export default function BarDetailPage() {
   const properties = [
     {
       field: "sport_prop",
-      icon: "${import.meta.env.BASE_URL}props/sport.svg",
+      icon: `${import.meta.env.BASE_URL}props/sport.svg`,
     },
     {
       field: "pool_prop",
-      icon: "${import.meta.env.BASE_URL}props/pool.svg",
+      icon: `${import.meta.env.BASE_URL}props/pool.svg`,
     },
     {
       field: "dart_prop",
-      icon: "${import.meta.env.BASE_URL}props/dart.svg",
+      icon: `${import.meta.env.BASE_URL}props/dart.svg`,
     },
     {
       field: "sitdown_prop",
-      icon: "${import.meta.env.BASE_URL}props/sitdown.svg",
+      icon: `${import.meta.env.BASE_URL}props/sitdown.svg`,
     },
     {
       field: "beer_prop",
-      icon: "${import.meta.env.BASE_URLprops/beer.svg",
+      icon: `${import.meta.env.BASE_URL}props/beer.svg`,
     },
     {
       field: "spil_prop",
-      icon: "${import.meta.env.BASE_URL}props/spil.svg",
+      icon: `${import.meta.env.BASE_URL}props/spil.svg`,
     },
     {
       field: "cocktails_prop",
-      icon: "${import.meta.env.BASE_URL}props/cocktails.svg",
+      icon: `${import.meta.env.BASE_URL}props/cocktails.svg`,
     },
     {
       field: "dj_prop",
-      icon: "${import.meta.env.BASE_URL}props/dj.svg",
+      icon: `${import.meta.env.BASE_URL}props/dj.svg`,
     },
     {
       field: "quiz_prop",
-      icon: "${import.meta.env.BASE_URL}props/quiz.svg",
+      icon: `${import.meta.env.BASE_URL}props/quiz.svg`,
     },
     {
       field: "floor_prop",
-      icon: "${import.meta.env.BASE_URL}props/floor.svg",
+      icon: `${import.meta.env.BASE_URL}props/floor.svg`,
     },
     {
       field: "studierabat_prop",
-      icon: "${import.meta.env.BASE_URL}props/studierabat.svg",
+      icon: `${import.meta.env.BASE_URL}props/studierabat.svg`,
     },
     {
       field: "livemusik_prop",
-      icon: "${import.meta.env.BASE_URL}props/livemusik.svg",
+      icon: `${import.meta.env.BASE_URL}props/livemusik.svg`,
     },
     {
       field: "karaoke_prop",
-      icon: "${import.meta.env.BASE_URL}props/karaoke.svg",
+      icon: `${import.meta.env.BASE_URL}props/karaoke.svg`,
     },
   ];
 
