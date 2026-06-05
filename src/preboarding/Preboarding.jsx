ad38af8cfb7f8ed95d4f99f204c26bfe5fae7f9b
millehlauridsen-ops <millehlauridsen@gmail.com>
2026-06-05 09:15:45 +0200

    Slettet step 6 fra preboarding

--- src/preboarding/Preboarding.jsx
+++ src/preboarding/Preboarding.jsx
@@ -4,7 +4,6 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
 import Step5 from "./Step5";
-import Step6 from "./Step6";
 import Step7 from "./Step7";
 
 export default function Preboarding({ onFinish }) {
@@ -21,7 +20,6 @@ export default function Preboarding({ onFinish }) {
       {step === 3 && <Step3 onNext={nextStep} />}
       {step === 4 && <Step4 onNext={nextStep} />}
       {step === 5 && <Step5 onNext={nextStep} />}
-      {step === 6 && <Step6 onNext={nextStep} />}
       {step === 7 && <Step7 onFinish={onFinish} />}
     </div>
   );
